43e6154aebd70ccf8f033badcc264abfc8b7bf7c
michael.geiss <michael.geiss@europace.de>
2017-06-06 15:47:12 +0200

    BotHandling erweitert: Filter Bot Messages und addList

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,44 +1,54 @@
 import {ITunesSlackBot} from "./ITunesSlackBot";
 import * as http from "http";
-import {ITunesDbService} from "./services/ITunesCouchService";
+import {ITunesDbService} from "./services/ITunesDbService";
 
-let dbService: ITunesDbService = new ITunesDbService(process.env.DB_HOST + ':' + process.env.DB_PORT, process.env.DB_NAME);
-const bot: ITunesSlackBot = new ITunesSlackBot(process.env.SLACK_BOT_ID, process.env.SLACK_BOT_NAME, process.env.SLACK_CHANNEL, dbService);
+
+
+
+    let dbService: ITunesDbService = new ITunesDbService(process.env.DB_HOST + ':' + process.env.DB_PORT, process.env.DB_NAME);
+    const bot: ITunesSlackBot = new ITunesSlackBot(process.env.SLACK_BOT_ID, process.env.SLACK_BOT_NAME, process.env.SLACK_CHANNEL, dbService);
 // new ElectronService();
 
-
-bot.checkePreisUpdate();
-setInterval(() => {
-  bot.checkePreisUpdate();
-}, 3600000);
-
-bot.handleUserInput(function (data: any) {
-
-  if (data.text === 'help') {
-    bot.poste('get \r\nadde ${url}')
-  }
-  else if (data.text === 'get') {
-    bot.posteAlleEintraege();
-  }
-  else if (data.text === 'check') {
-    bot.poste('Prüfung wird durchgeführt')
     bot.checkePreisUpdate();
-    // setInterval(() => {
-    //   bot.checkePreisUpdate();
-    // }, 600000)
-  }
-  else if (data.text.startsWith('adde')) {
-    dbService.insertUrlToDb(data.text.substring(5)).then(
-      (successUrl: string) => bot.poste(`${successUrl} erfolgreich eingetragen`)
-    )
-  }
-  else {
-    bot.poste('Ich verstehe dich nicht :stuck_out_tongue_winking_eye:');
-  }
+    setInterval(() => {
+      bot.checkePreisUpdate();
+    }, 3600000);
 
-});
+    bot.handleUserInput(function (data: any) {
+      if (data.bot_id) {
+        return;
+      }
+      else if (data.text === 'help') {
+        bot.poste('get \r\nadde ${url}')
+      }
+      else if (data.text === 'get') {
+        bot.posteAlleEintraege();
+      }
+      else if (data.text === 'check') {
+        bot.poste('Prüfung wird durchgeführt')
+        bot.checkePreisUpdate();
+        // setInterval(() => {
+        //   bot.checkePreisUpdate();
+        // }, 600000)
+      }
+      else if (data.text.startsWith('adde')) {
+        dbService.insertUrlToDb(data.text.substring(5)).then(
+          (successUrl: string) => bot.poste(`${successUrl} erfolgreich eingetragen`)
+        )
+      }
+      else if (data.text.startsWith('addList')) {
+        let input: string = data.text.substring(8);
+        input.split(' ').forEach((id) => {
+          dbService.insertUrlToDb(id).then(
+            (successUrl: string) => bot.poste(`${successUrl} erfolgreich eingetragen`)
+          )
+        })
 
-
+      }
+      else {
+        bot.poste('Ich verstehe dich nicht :stuck_out_tongue_winking_eye:');
+      }
+    });
 
 // http.createServer(function (request, response) {
 //
